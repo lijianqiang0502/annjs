aabf6a49d9a336bcea6398765b2b72af057f016a
lijianqiang01 <lijianqiang01@baidu.com>
2020-09-02 11:51:15 +0800

    init: 多个单元测试任务

--- src/object-to-query/index.test.js
+++ src/object-to-query/index.test.js
@@ -1,6 +1,12 @@
 import objectToQuery from './index'
 
 
-test('test objectToQuery', () => {
-  expect(objectToQuery({a:1,b:2})).toEqual("a=1&b=2");
-});
+describe('test objectToQuery',() => {
+    it('test one', () => {
+      expect(objectToQuery({a:"231421412",b:2})).toEqual("a=231421412&b=2");
+    })
+    it('test two', () => {
+      expect(objectToQuery({a:3214214,b:421})).toEqual("a=3214214&b=421");
+    })
+})
+
