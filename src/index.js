02f697fca3f4138e6d0714be6ae3c48097a9d90f
lijianqiang01 <lijianqiang01@baidu.com>
2020-09-22 11:23:11 +0800

    init: 简单的实现触底加载

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import sort from './sort';
 import objToQuery from './object-to-query';
 import queryToObj from './query-to-object';
 import LazyImage from './lazy-image';
+import LazyLoad from './lazy-load';
 import random from './random';
 import debounce from './debounce';
 
@@ -10,6 +11,7 @@ export {
   objToQuery,
   queryToObj,
   LazyImage,
+  LazyLoad,
   debounce,
   random,
 }
---
+++ src/lazy-load/index.js
@@ -0,0 +1,26 @@
+export default function LazyLoad (selector, bottomCallback, delay = 1000) {
+    this.target = document.querySelector(selector);
+    this.listen = (item) => {
+        let options = {
+            rootMargin: '0px',
+            threshold: 0.01,
+        };
+        let callback = (entries) => {
+            if (entries[0].isIntersecting) {
+                bottomCallback();
+                observer.disconnect()
+            }
+
+        }
+        let observer = new IntersectionObserver(callback, options);
+
+        setInterval(()=> {
+            observer.observe(this.target.lastElementChild);
+        },delay)
+    }
+
+    this.init = () => {
+        this.listen(this.target);
+    }
+}
+
